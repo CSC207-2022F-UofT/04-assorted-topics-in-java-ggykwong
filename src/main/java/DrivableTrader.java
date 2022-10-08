e1585d7d9c240ecf7936f314737aef23e62bf11d
gwendolynkwong <gwen.kwong1@gmail.com>
2022-10-08 19:30:28 -0400

    Completed some tasks

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -10,4 +10,59 @@
  * Look at DomesticatableTrader.java for an example.
  */
 
+import java.util.ArrayList;
 import java.util.List;
+
+public class DrivableTrader<Drivable> extends Trader<Object> {
+    private final List<Drivable> inventory;
+    private final List<Drivable> wishlist;
+    private int money;
+
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super((List<Object>) inventory, (List<Object>) wishlist, money);
+        this.inventory = inventory;
+        this.wishlist = wishlist;
+        this.money = money;
+    }
+
+    public DrivableTrader(int money) {
+        super(money);
+        this.inventory = new ArrayList<Drivable>();
+        this.wishlist = new ArrayList<Drivable>();
+    }
+
+    public void addToWishlist(Object item) {
+        this.wishlist.add((Drivable) item);
+    }
+
+    public int getSellingPrice(Object item) {
+        if (item instanceof Tradable) {
+            return ((Tradable) item).getPrice() + item.getMaxSpeed();
+        }
+        return Tradable.MISSING_PRICE;
+    }
+
+    public boolean exchangeMoney(Trader<Object> other, Object item) {
+        return super.exchangeMoney(other, item);
+    }
+
+    public boolean sellTo(Trader<Object> other) {
+        return super.sellTo(other);
+    }
+
+    public boolean buyFrom(Trader<Object> other) {
+        return super.buyFrom(other);
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+
+    public List<Object> getWishlist(){
+        return super.getWishlist();
+    }
+
+    public List<Object> getInventory() {
+        return super.getInventory();
+    }
+}
